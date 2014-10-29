12ed2fa95dd58d4cce51c3c4ccdf133079c839bc
Mike Kooistra <dragonsoft_nl@hotmail.com>
2014-10-29 16:07:33 +0100

    recommit
    rrr

--- Barroc/Barroc/Project.cs
+++ Barroc/Barroc/Project.cs
@@ -59,6 +59,7 @@ namespace Barroc
             this.Size = new Size(650, 300);
         }
 
+
         private void btn_toevoegen_Click(object sender, EventArgs e)
         {
             string sql = "INSERT INTO Projects (Project_ID, Customer_ID, Project_name, Status, Applications, Hardware, Software, Offer_number, Internal_contact_person, Start_date, End_date, Nmber_of_invoices) values (@Project_ID,@Customer_ID,@Project_name,@Status,@Applications,@Hardware,@Software,@Offer_number,@Internal_contact_person,@Start_date,@End_date,@Nmber_of_invoices)";
